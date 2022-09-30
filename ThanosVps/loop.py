22ccbb1d4b51ce0cf467ba8ecc5e7846dd4ddb69
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-09-30 08:56:57 -0700

    Update loop.py

--- ThanosVps/loop.py
+++ ThanosVps/loop.py
@@ -1,5 +1,5 @@
 thanos username:"rishabhxd2" ^GIT_CLONER=thanos-vps
-  thanos="rishabhxd2" ^GIT):base program await 
+  thanos="THANOS-VPS" ^GIT):base program await 
                        def loop server 24H.runtime return
     try()
     thanos-vps{{[GITHUB]}}.github/workflows/server.loop
